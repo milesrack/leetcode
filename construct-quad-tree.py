89147b3d52530109bf2199dcf8748ef8cf7b1c60
user <user@example.com>
2023-03-04 01:24:22 -0500

    Finish construct quad tree

--- construct-quad-tree.py
+++ construct-quad-tree.py
@@ -8,28 +8,24 @@ class Node:
         self.bottomRight = bottomRight
 
 class Solution:
-    def construct(self, grid: list[list[int]]) -> 'Node':
-        n = len(grid)
-        print(grid)
-        val = grid[0][0]
-        isLeaf = True
-        
-        for i in range(n):
-            for j in range(len(grid[i])):
-                if val != grid[i][j]:
-                    isLeaf = False                    
-                val = grid[i][j]
-                
-        if isLeaf:
-            node = Node(val, isLeaf, None, None, None, None)
+    def same(self, grid, x, y, n):
+        for i in range(x, x + n):
+            for j in range(y, y + n):
+                if grid[x][y] != grid[i][j]:
+                    return False                    
+        return True
+    
+    def helper(self, grid, x, y, n):
+        #print(grid)
+        if self.same(grid, x, y, n):
+            node = Node(grid[x][y] == 1, True, None, None, None, None)
         else:
-            # TODO : Subdivide the grids
-            topLeft = self.construct(grid[:n//2][:n//2])
-            topRight = self.construct(grid[:n//2][n//2:])
-            bottomLeft = self.construct(grid[n//2:][:n//2])
-            bottomRight = self.construct(grid[n//2:][n//2:])
-            node = Node(val, isLeaf, topLeft, topRight, bottomLeft, bottomRight)
+            topLeft = self.helper(grid, x, y, n//2)
+            topRight = self.helper(grid, x, y + n//2, n//2)
+            bottomLeft = self.helper(grid, x + n//2, y, n//2)
+            bottomRight = self.helper(grid, x + n//2, y + n//2, n//2)
+            node = Node(False, False, topLeft, topRight, bottomLeft, bottomRight)
         return node
 
-s = Solution()
-n = s.construct([[0,1],[1,0]])
+    def construct(self, grid: list[list[int]]) -> 'Node':
+        return self.helper(grid, 0, 0, len(grid))
